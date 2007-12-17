eb96117989453450041a8211c0edceff6db9e8d4
castano <castano@95f4ed2b-212e-0410-8b90-d31948207fce>
2007-12-17 05:26:14 +0000

    Add ui tool temporarily called main...

--- src/nvtt/tools/configdialog.cpp
+++ src/nvtt/tools/configdialog.cpp
@@ -23,9 +23,138 @@
 
 #include "configdialog.h"
 
+#include <QtCore/QDebug>
+
+#include <QtGui/QImage>
+
 
 ConfigDialog::ConfigDialog(QWidget *parent/*=0*/) : QDialog(parent)
 {
-	ui.setupUi(this);
+	init();
 }
+
+ConfigDialog::ConfigDialog(const char * fileName, QWidget *parent/*=0*/) : QDialog(parent)
+{
+	init();
+	
+	open(fileName);
+}
+
+void ConfigDialog::init()
+{
+	ui.setupUi(this);
+	
+	connect(ui.openButton, SIGNAL(clicked()), this, SLOT(openClicked()));
+	connect(ui.generateMipmapsCheckBox, SIGNAL(stateChanged(int)), this, SLOT(generateMipmapsChanged(int)));
+	connect(ui.mipmapFilterComboBox, SIGNAL(activated(QString)), this, SLOT(mipmapFilterChanged(QString)));
+	//connect(ui.mipmapFilterSettings, SIGNAL(clicked()), this, SLOT(mipmapFilterSettingsShow()));
+	
+	connect(ui.redSpinBox, SIGNAL(valueChanged(double)), this, SLOT(colorWeightChanged()));
+	connect(ui.greenSpinBox, SIGNAL(valueChanged(double)), this, SLOT(colorWeightChanged()));
+	connect(ui.blueSpinBox, SIGNAL(valueChanged(double)), this, SLOT(colorWeightChanged()));
+	connect(ui.uniformButton, SIGNAL(toggled(bool)), this, SLOT(uniformWeightToggled(bool)));
+	connect(ui.luminanceButton, SIGNAL(toggled(bool)), this, SLOT(luminanceWeightToggled(bool)));
+}
+
+
+void ConfigDialog::openClicked()
+{
+	// @@ Open file dialog.
+	
+	QString fileName;
+	
+	open(fileName);
+}
+
+void ConfigDialog::generateMipmapsChanged(int state)
+{
+	Q_UNUSED(state);
+	
+	bool generateMipmapEnabled = ui.generateMipmapsCheckBox->isChecked();
+	
+	ui.mipmapFilterLabel->setEnabled(generateMipmapEnabled);
+	ui.mipmapFilterComboBox->setEnabled(generateMipmapEnabled);
+	ui.limitMipmapsCheckBox->setEnabled(generateMipmapEnabled);
+	
+	bool enableFilterSettings = (ui.mipmapFilterComboBox->currentText() == "Kaiser");
+	ui.mipmapFilterSettings->setEnabled(generateMipmapEnabled && enableFilterSettings);
+	
+	bool enableMaxLevel = ui.limitMipmapsCheckBox->isChecked();
+	ui.maxLevelLabel->setEnabled(generateMipmapEnabled && enableMaxLevel);
+	ui.maxLevelSpinBox->setEnabled(generateMipmapEnabled && enableMaxLevel);
+}
+
+void ConfigDialog::mipmapFilterChanged(QString name)
+{
+	bool enableFilterSettings = (name == "Kaiser");
+	ui.mipmapFilterSettings->setEnabled(enableFilterSettings);
+}
+
+
+void ConfigDialog::colorWeightChanged()
+{
+	double r = ui.redSpinBox->value();
+	double g = ui.greenSpinBox->value();
+	double b = ui.blueSpinBox->value();
+	
+	bool uniform = (r == 1.0 && g == 1.0 && b == 1.0);
+	bool luminance = (r == 0.3 && g == 0.59 && b == 0.11);
+	
+	ui.uniformButton->setChecked(uniform);
+	ui.luminanceButton->setChecked(luminance);
+}
+
+void ConfigDialog::uniformWeightToggled(bool checked)
+{
+	if (checked)
+	{
+		ui.redSpinBox->setValue(1.0);
+		ui.greenSpinBox->setValue(1.0);
+		ui.blueSpinBox->setValue(1.0);
+	}
+}
+
+void ConfigDialog::luminanceWeightToggled(bool checked)
+{
+	if (checked)
+	{
+		ui.redSpinBox->setValue(0.3);
+		ui.greenSpinBox->setValue(0.59);
+		ui.blueSpinBox->setValue(0.11);
+	}
+}
+
+
+
+
+bool ConfigDialog::open(QString fileName)
+{
+	// @@ Load image.
+	QImage image;
+	
+	// @@ If success.
+	{
+		ui.imagePathLineEdit->setText(fileName);
+		
+		// @@ Set image in graphics view.
+		
+		// @@ Set image description.
+		
+		// @@ Provide image to nvtt.
+		
+		int w = image.width();
+		int h = image.height();
+		void * data = NULL;
+		
+		inputOptions.setTextureLayout(nvtt::TextureType_2D, w, h);
+		inputOptions.setMipmapData(data, w, h);
+		
+		return true;
+	}
+
+	return false;
+}
+
+
+
 
