e067ba8ff92e2280e316877375e5473d8f34d065
castano <castano@95f4ed2b-212e-0410-8b90-d31948207fce>
2008-10-01 22:28:57 +0000

    Set correct DXT5n swizzle code. Select swizzle codes in nvtt instead of nvimage.

--- trunk/src/nvimage/DirectDrawSurface.h
+++ trunk/src/nvimage/DirectDrawSurface.h
@@ -94,6 +94,7 @@ namespace nv
 		void setPitch(uint pitch);
 		void setFourCC(uint8 c0, uint8 c1, uint8 c2, uint8 c3);
 		void setFormatCode(uint code);
+		void setSwizzleCode(uint8 c0, uint8 c1, uint8 c2, uint8 c3);
 		void setPixelFormat(uint bitcount, uint rmask, uint gmask, uint bmask, uint amask);
 		void setDX10Format(uint format);
 		void setNormalFlag(bool b);
