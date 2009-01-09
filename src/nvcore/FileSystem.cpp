10de10b9c21d37bba06c10e062d2c0f3d755577a
castano <castano@95f4ed2b-212e-0410-8b90-d31948207fce>
2009-01-09 02:24:32 +0000

    Implement FileSystem::exists().

--- src/nvcore/FileSystem.cpp
+++ src/nvcore/FileSystem.cpp
@@ -8,7 +8,7 @@
 #else
 #include <sys/stat.h>
 #include <sys/types.h>
-//#include <unistd.h>
+#include <unistd.h>
 #endif
 
 using namespace nv;
@@ -16,9 +16,17 @@ using namespace nv;
 
 bool FileSystem::exists(const char * path)
 {
-//   stat buf;
-//   stat(path, &buf);
-    return false;
+#if NV_OS_UNIX
+	struct stat buf;
+	return stat(path, &buf) == 0;
+#else
+	if (FILE * fp = fopen(path, "r"))
+	{
+		fclose(fp);
+		return true;
+	}
+	return false;
+#endif
 }
 
 bool FileSystem::createDirectory(const char * path)
@@ -29,3 +37,4 @@ bool FileSystem::createDirectory(const char * path)
     return mkdir(path, 0777) != -1;
 #endif
 }
+
