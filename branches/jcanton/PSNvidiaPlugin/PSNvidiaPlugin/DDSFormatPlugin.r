b2760982cf1a25be614495650c0e2b9fde8bf5b4
javiuniversidad@gmail.com <javiuniversidad@gmail.com@95f4ed2b-212e-0410-8b90-d31948207fce>
2009-03-09 15:48:12 +0000

    Improved PIPL resource.

--- branches/jcanton/PSNvidiaPlugin/PSNvidiaPlugin/DDSFormatPlugin.r
+++ branches/jcanton/PSNvidiaPlugin/PSNvidiaPlugin/DDSFormatPlugin.r
@@ -41,54 +41,28 @@ resource 'PiPL' (16000, "NvidiaPlugin PiPL", purgeable)
 		Name 				{ "NvidiaPlugin" },
 		Version 			{ (latestFormatVersion << 16) | latestFormatSubVersion },		
 
-#if MSWindows
 		CodeWin32X86       { "PluginMain" },
-#elif TARGET_API_MAC_CARBON
-		CodeCarbonPowerPC	{ 0, 0, "" },
-#else
-        CodePowerPC 		{ 0, 0, "" },
-#endif
 		
-		FmtFileType 		{ 'EXR ', '8BIM' },
-		ReadTypes 			{ { 'EXR ', '    ' } },
-		FilteredTypes 		{ { 'EXR ', '    ' } },
-		ReadExtensions 		{ { 'exr ' } },
-		WriteExtensions 	{ { 'exr ' } },
-		FilteredExtensions 	{ { 'exr ' } },
+		FmtFileType 		{ 'DDS ', '8BIM' },
+		ReadTypes 			{ { 'DDS ', '    ' } },
+		FilteredTypes 		{ { 'DDS ', '    ' } },
+		ReadExtensions 		{ { 'dds ' } },
+		WriteExtensions 	{ { 'dds ' } },
+		FilteredExtensions 	{ { 'dds ' } },
 		FormatMaxSize		{ { 32767, 32767 } },
-
 		
-		// this is to make us available when saving a 16-bit image  
-
-		EnableInfo          
-		{ 
-			"in (PSHOP_ImageMode, RGBMode, RGB48Mode)"		
-		},      
-
-
-		// this is apparently just for backwards compatability
-
 		SupportedModes
 		{
 			noBitmap,
 			noGrayScale,
 			noIndexedColor,
-			doesSupportRGBColor,	// yes
+			doesSupportRGBColor,
 			noCMYKColor,
 			noHSLColor,
 			noHSBColor,
 			noMultichannel,
 			noDuotone,
 			noLABColor
-#if !MSWindows			
-			,
-			noGray16,
-			doesSupportRGB48,		// yes
-			noLab48,
-			noCMYK64,
-			noDeepMultichannel,
-			noDuotone16
-#endif
 		},
 
 		FormatFlags 
@@ -97,30 +71,10 @@ resource 'PiPL' (16000, "NvidiaPlugin PiPL", purgeable)
 			fmtCanRead, 
 			fmtCanWrite, 
 			fmtCanWriteIfRead, 
-			fmtCannotWriteTransparency
-#if MSWindows			
-			,
+			fmtCannotWriteTransparency,
 			fmtCannotCreateThumbnail
-#endif
 		},	
 		
-		FormatMaxChannels 
-		{ 
-			{
-				1, 
-				4, 
-				4, 
-				4, 
-				4, 
-				4, 
-				4, 
-				4, 
-				4, 
-				4, 
-				4, 
-				4
-			}
-		}
 	}
 };
 
