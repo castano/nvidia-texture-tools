fd2492670ecfa9ee323d3d2abffa742828e4eba4
castano <castano@95f4ed2b-212e-0410-8b90-d31948207fce>
2009-07-06 08:57:36 +0000

    Add a QPF timer.

--- src/nvcore/Timer.h
+++ src/nvcore/Timer.h
@@ -5,6 +5,8 @@
 
 #include <nvcore/nvcore.h>
 
+#if 1
+
 #include <time.h> //clock
 
 class NVCORE_CLASS Timer
@@ -13,10 +15,46 @@ public:
 	Timer() {}
 	
 	void start() { m_start = clock(); }
-	int elapsed() const { return (1000 * (clock() - m_start)) / CLOCKS_PER_SEC; }
+    void stop() { m_stop = clock(); }
+
+	int elapsed() const { return (1000 * (m_stop - m_start)) / CLOCKS_PER_SEC; }
 	
 private:
 	clock_t m_start;
+    clock_t m_stop;
 };
+
+#else
+
+#define WINDOWS_LEAN_AND_MEAN
+#define VC_EXTRALEAN
+#define NOMINMAX
+#include <windows.h>
+
+class NVCORE_CLASS Timer
+{
+public:
+	Timer() {
+        // get the tick frequency from the OS
+        QueryPerformanceFrequency((LARGE_INTEGER*) &m_frequency);
+    }
+	
+	void start() { QueryPerformanceCounter((LARGE_INTEGER*) &m_start); }
+    void stop() { QueryPerformanceCounter((LARGE_INTEGER*) &m_stop); }
+
+	int elapsed() const {
+        return (int)1000 * ((double)m_stop.QuadPart - (double)m_start.QuadPart) / (double)m_frequency.QuadPart;
+    }
+	
+private:
+    LARGE_INTEGER  m_frequency;
+    LARGE_INTEGER  m_start;
+    LARGE_INTEGER  m_stop;
+
+};
+
+#endif // 0
+
+
 
 #endif // NV_CORE_TIMER_H
