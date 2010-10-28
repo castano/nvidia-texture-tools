f33bcfafaddef2095d3c699a297c144b599c271d
castano <castano@95f4ed2b-212e-0410-8b90-d31948207fce>
2010-10-28 04:25:23 +0000

    Remove malloc overrides. Fixes issue 138.

--- src/nvtt/CompressRGB.cpp
+++ src/nvtt/CompressRGB.cpp
@@ -82,7 +82,7 @@ void nv::compressRGB(const Image * image, const OutputOptions::Private & outputO
     // Determine pitch.
     uint pitch = computePitch(w, compressionOptions.bitcount, 8);
 
-    uint8 * dst = (uint8 *)mem::malloc(pitch + 4);
+    uint8 * dst = (uint8 *)::malloc(pitch + 4);
 
     for (uint y = 0; y < h; y++)
     {
@@ -127,6 +127,6 @@ void nv::compressRGB(const Image * image, const OutputOptions::Private & outputO
         }
     }
 
-    mem::free(dst);
+    ::free(dst);
 }
 
