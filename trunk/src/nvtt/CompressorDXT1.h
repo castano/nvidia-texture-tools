7328f231880ccd0ac91255436dd5a3ad222b0c0c
castano <castano@95f4ed2b-212e-0410-8b90-d31948207fce>
2014-12-02 05:43:13 +0000

    DXT1 compressor tweaks.

--- trunk/src/nvtt/CompressorDXT1.h
+++ trunk/src/nvtt/CompressorDXT1.h
@@ -28,11 +28,12 @@ namespace nv {
     float compress_dxt1_single_color_optimal(Color32 c, BlockDXT1 * output);
     float compress_dxt1_single_color_optimal(const Vector3 & color, BlockDXT1 * output);
 
-    float compress_dxt1_least_squares_fit(const Vector3 input_colors[16], const Vector3 * colors, const float * weights, int count, BlockDXT1 * output);
-    float compress_dxt1_bounding_box_exhaustive(const Vector3 input_colors[16], const Vector3 * colors, const float * weights, int count, int search_limit, BlockDXT1 * output);
-    float compress_dxt1_cluster_fit(const Vector3 input_colors[16], const Vector3 * colors, const float * weights, int count, BlockDXT1 * output);
+    float compress_dxt1_single_color(const Vector3 * colors, const float * weights, int count, const Vector3 & color_weights, BlockDXT1 * output);
+    float compress_dxt1_least_squares_fit(const Vector3 input_colors[16], const Vector3 * colors, const float * weights, int count, const Vector3 & color_weights, BlockDXT1 * output);
+    float compress_dxt1_bounding_box_exhaustive(const Vector3 input_colors[16], const Vector3 * colors, const float * weights, int count, const Vector3 & color_weights, int search_limit, BlockDXT1 * output);
+    void compress_dxt1_cluster_fit(const Vector3 input_colors[16], const Vector3 * colors, const float * weights, int count, const Vector3 & color_weights, BlockDXT1 * output);
 
 
-    float compress_dxt1(const Vector3 colors[16], const float weights[16], BlockDXT1 * output);
+    float compress_dxt1(const Vector3 colors[16], const float weights[16], const Vector3 & color_weights, BlockDXT1 * output);
 
 }
