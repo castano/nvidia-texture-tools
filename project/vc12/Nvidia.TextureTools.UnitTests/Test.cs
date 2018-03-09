cdf0df330712d1d5172a704e8f5403ac108c7f8b
Dean Ellis <dellis1972@googlemail.com>
2018-03-09 20:15:06 +0000

    Added C# Unit Tests

---
+++ project/vc12/Nvidia.TextureTools.UnitTests/Test.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using Nvidia.TextureTools;
+namespace Nvidia.TextureTools.UnitTests {
+	[TestFixture ()]
+	public class Test {
+		[Test ()]
+		public void TestCase ()
+		{
+			var inputOptions = new InputOptions ();
+			var outputOptions = new OutputOptions ();
+			var compressionOptions = new CompressionOptions ();
+			var compressor = new Compressor ();
+
+			compressor.Compress (inputOptions, compressionOptions, outputOptions);
+		}
+	}
+}
