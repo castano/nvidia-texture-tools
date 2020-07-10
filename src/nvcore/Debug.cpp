castano/nvidia-texture-tools

commit 831d8e1a9173b1fb7e01f10fc1d33f42fe2da0fd

Fix build on AArch64. Fixes #298.

src/nvcore/Debug.cpp
@@ -1015,6 +1015,12 @@ void debug::dumpInfo()
 #endif
 }
 
+static void getEmptyVAList(va_list& list, ...)
+{
+    va_start(list, list);
+    va_end(list);
+}
+
 /// Dump callstack using the specified handler.
 void debug::dumpCallstack(MessageHandler *messageHandler, int callstackLevelsToSkip /*= 0*/)
 {
@@ -1027,8 +1033,11 @@ void debug::dumpCallstack(MessageHandler *messageHandler, int callstackLevelsToS
         Array<const char *> lines;
         writeStackTrace(trace, size, callstackLevelsToSkip + 1, lines);     // + 1 to skip the call to dumpCallstack
 
+        va_list empty;
+        getEmptyVAList(empty);
+
         for (uint i = 0; i < lines.count(); i++) {
-            messageHandler->log(lines[i], NULL);
+            messageHandler->log(lines[i], empty);
             delete lines[i];
         }
     }

src/nvcore/Debug.h
@@ -166,7 +166,7 @@ NVCORE_API void NV_CDECL nvDebugPrint( const char *msg, ... ) __attribute__((for
 namespace nv
 {
     inline bool isValidPtr(const void * ptr) {
-    #if NV_CPU_X86_64 || POSH_CPU_PPC64
+    #if NV_CPU_X86_64 || POSH_CPU_PPC64 || NV_CPU_AARCH64
         if (ptr == NULL) return true;
         if (reinterpret_cast<uint64>(ptr) < 0x10000ULL) return false;
         if (reinterpret_cast<uint64>(ptr) >= 0x000007FFFFFEFFFFULL) return false;

commit d73335fb82f8e59c3b9a9fca79a9e3bdb7d1caa4

Fix build on VC++. Refs #309.
This was broken by 831d8e1a9173b1fb7e01f10fc1d33f42fe2da0fd.

src/nvcore/Debug.cpp
@@ -1015,10 +1015,11 @@ void debug::dumpInfo()
 #endif
 }
 
-static void getEmptyVAList(va_list& list, ...)
+static va_list getEmptyVAList(va_list list, ...)
 {
     va_start(list, list);
     va_end(list);
+    return list;
 }
 
 /// Dump callstack using the specified handler.
@@ -1034,7 +1035,7 @@ void debug::dumpCallstack(MessageHandler *messageHandler, int callstackLevelsToS
         writeStackTrace(trace, size, callstackLevelsToSkip + 1, lines);     // + 1 to skip the call to dumpCallstack
 
         va_list empty;
-        getEmptyVAList(empty);
+        empty = getEmptyVAList(empty);
 
         for (uint i = 0; i < lines.count(); i++) {
             messageHandler->log(lines[i], empty);